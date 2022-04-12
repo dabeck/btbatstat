aaeb8f150a0e8d81e31f4da5646e5f3d8995449e
Daniel Strufe <db@fino.digital>
2022-04-12 14:17:39 +0200

    Migrate to python3

--- BtBatStat.py
+++ BtBatStat.py
@@ -1,4 +1,4 @@
-import subprocess,re,time,sys,webbrowser,urllib2,decimal
+import subprocess,re,time,sys,webbrowser,urllib.request,urllib.error,urllib.parse,decimal
 from Foundation import NSDate,NSObject,NSTimer,NSRunLoop,NSDefaultRunLoopMode
 from AppKit import NSImage,NSStatusBar,NSMenuItem,NSApplication,NSMenu,NSVariableStatusItemLength,NSRunAlertPanel
 from PyObjCTools import AppHelper
@@ -7,7 +7,7 @@ from optparse import OptionParser
 if len(sys.argv) > 1 and sys.argv[1][:4] == '-psn':
   del sys.argv[1]
 
-VERSION = '0.9.1'
+VERSION = '0.10.0'
 LONGVERSION = 'BtBatStat ' + VERSION
 
 AboutText = """Writen by: Joris Vandalon
@@ -30,24 +30,24 @@ start_time = NSDate.date()
 
 def versionCheck():
     try:
-	LatestRelease = urllib2.urlopen("http://btbatstat.vandalon.org/VERSION", None, 2).read().strip()
+      LatestRelease = urllib.request.urlopen("http://btbatstat.vandalon.org/VERSION", None, 2).read().strip()
     except:
-	return False
-    return ( LatestRelease and map(int, LatestRelease.split('.')) > map(int, VERSION.split('.')) )
+      return False
+    return ( LatestRelease and list(map(int, LatestRelease.split('.'))) > list(map(int, VERSION.split('.'))) )
 
 #Check for new version
 def checkForUpdates():
     if versionCheck():
-	if NSRunAlertPanel(LONGVERSION, updateText , "Download Update", "Ignore for now", None ) == 1:
-	    webbrowser.open(updateUrl)
+      if NSRunAlertPanel(LONGVERSION, updateText , "Download Update", "Ignore for now", None ) == 1:
+        webbrowser.open(updateUrl)
 
 class Timer(NSObject):
   def about_(self, notification):
     if versionCheck():
-	AboutTitle = LONGVERSION + " (Update Available!)"
-        about = NSRunAlertPanel(AboutTitle, AboutText , "OK", "Visit Website", "Download Update" )
+      AboutTitle = LONGVERSION + " (Update Available!)"
+      about = NSRunAlertPanel(AboutTitle, AboutText , "OK", "Visit Website", "Download Update" )
     else:
-        about = NSRunAlertPanel(LONGVERSION, AboutText , "OK", "Visit Website", None )
+      about = NSRunAlertPanel(LONGVERSION, AboutText , "OK", "Visit Website", None )
     if about == 0:
       webbrowser.open(appUrl)
     elif about == -1:
@@ -64,9 +64,9 @@ class Timer(NSObject):
     # Load images
     self.noDeviceImage = NSImage.alloc().initByReferencingFile_('icons/no_device.png')
     self.barImage = dict(kb = NSImage.alloc().initByReferencingFile_('icons/kb.png'),
-			 magicMouse = NSImage.alloc().initByReferencingFile_('icons/magic_mouse.png'),
-			 mightyMouse = NSImage.alloc().initByReferencingFile_('icons/mighty_mouse.png'),
-			 magicTrackpad = NSImage.alloc().initByReferencingFile_('icons/TrackpadIcon.png'))
+        magicMouse = NSImage.alloc().initByReferencingFile_('icons/magic_mouse.png'),
+        mightyMouse = NSImage.alloc().initByReferencingFile_('icons/mighty_mouse.png'),
+        magicTrackpad = NSImage.alloc().initByReferencingFile_('icons/TrackpadIcon.png'))
 
     #Define menu items
     self.statusbar = NSStatusBar.systemStatusBar()
@@ -101,47 +101,47 @@ class Timer(NSObject):
     self.devicesFound = 0
 
     if options.debug:
-	start = time.time()
+      start = time.time()
 
     deviceCmd = dict( mightyMouse = self.ioreg("AppleBluetoothHIDMouse","-rc"),
-	 	      magicMouse = self.ioreg("BNBMouseDevice","-rc"),
-	 	      magicTrackpad = self.ioreg("BNBTrackpadDevice","-rc"))
+        magicMouse = self.ioreg("BNBMouseDevice","-rc"),
+        magicTrackpad = self.ioreg("BNBTrackpadDevice","-rc"))
     deviceCmd['kb'] = self.ioreg("AppleBluetoothHIDKeyboard","-rc")
     if not deviceCmd['kb']:
         deviceCmd['kb'] = self.ioreg("IOAppleBluetoothHIDDriver","-n")
 
-    for device,Output in deviceCmd.items():
-	Percentage = 0
-	if Output:
-	    Percentage = re.search('BatteryPercent" = (\d{1,2}0?)', Output)
-	    if Percentage:
-		self.hit = True
-		if options.debug:
-		    print "Found " + device
-		self.devicesFound += 1
-		if self.noDevice is not None:
-		    self.statusbar.removeStatusItem_(self.noDevice)
-        	    self.menu.removeItem_(self.menuNotice)
-		    self.noDevice = None
-		if not device in self.barItem:
-		    self.barItem[device] = self.createBarItem(self.barImage[device])
-		self.barItem[device].setTitle_(Percentage.group(1) + '%')
+    for device,Output in list(deviceCmd.items()):
+      Percentage = 0
+      if Output:
+        Percentage = re.search('BatteryPercent" = (\d{1,2}0?)', Output)
+        if Percentage:
+          self.hit = True
+          if options.debug:
+            print("Found " + device)
+          self.devicesFound += 1
+          if self.noDevice is not None:
+            self.statusbar.removeStatusItem_(self.noDevice)
+            self.menu.removeItem_(self.menuNotice)
+            self.noDevice = None
+          if not device in self.barItem:
+            self.barItem[device] = self.createBarItem(self.barImage[device])
+          self.barItem[device].setTitle_(Percentage.group(1) + '%')
 
-	if device in self.barItem and not Percentage:
-	    	self.statusbar.removeStatusItem_(self.barItem[device])
-		del self.barItem[device]
+      if device in self.barItem and not Percentage:
+            self.statusbar.removeStatusItem_(self.barItem[device])
+            del self.barItem[device]
     
     if options.debug:
-	print "Found", self.devicesFound, "Devices."
+      print("Found", self.devicesFound, "Devices.")
 
     if self.devicesFound == 0 and self.noDevice == None:
-        self.menuNotice = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('BtBatStat: No devices found.', '', '')
-        self.menu.insertItem_atIndex_(self.menuNotice,0)
-	self.noDevice = self.createBarItem(self.noDeviceImage)
+      self.menuNotice = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_('BtBatStat: No devices found.', '', '')
+      self.menu.insertItem_atIndex_(self.menuNotice,0)
+      self.noDevice = self.createBarItem(self.noDeviceImage)
 
     if options.debug:
-	end = time.time()
-	print "Time elapsed = ", end - start, "seconds"
+      end = time.time()
+      print("Time elapsed = ", end - start, "seconds")
 
 if __name__ == "__main__":
   app = NSApplication.sharedApplication()
